1a94dea696e93b1ea6f2c49d0c5ff8ab46a097d7
anushree <anushree812001@gmail.com>
2022-05-18 07:12:28 +0530

    Sendind 2nd file fron local to github

---
+++ TestingCommitLocalGithub/src/TestingNewCommit.java
@@ -0,0 +1,10 @@
+
+public class TestingNewCommit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing new commit after 1st commit in the project.");
+
+	}
+
+}
